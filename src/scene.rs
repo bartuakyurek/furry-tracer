4e436f0171d42b479d7e4e819cf5b5d93856dfcc
Bartu <bartuakyurek@outlook.com>
2025-10-02 16:15:23 +0300

    Add position to Camera

--- src/scene.rs
+++ src/scene.rs
@@ -11,8 +11,8 @@ CENG 795's JSON file formats.
 */
 
 use serde::{Deserialize};
-use crate::numeric::{RGBColor, Int, Float};
-use crate::json_parser::{deser_int, deser_float, deser_vec3_from_str};
+use crate::numeric::{RGBColor, Int, Float, Vector3};
+use crate::json_parser::{deser_int, deser_float, deser_vec3, deser_dvec3};
 
 #[derive(Debug, Deserialize)]
 pub struct RootScene {
@@ -24,7 +24,7 @@ pub struct Scene {
     #[serde(deserialize_with = "deser_int")]
     MaxRecursionDepth: Int,
 
-    #[serde(deserialize_with = "deser_vec3_from_str")]
+    #[serde(deserialize_with = "deser_vec3")]
     BackgroundColor: RGBColor,
 
     #[serde(deserialize_with = "deser_float")]
@@ -67,7 +67,9 @@ impl Cameras {
 struct Camera {
     #[serde(rename = "_id", deserialize_with = "deser_int")]
     id: Int,
-
+    
+    #[serde(rename = "Position", deserialize_with = "deser_dvec3")]
+    position: Vector3,
 
 }
 
