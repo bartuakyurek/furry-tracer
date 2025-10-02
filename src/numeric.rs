e6ab571348f8345baf2f11312cb925e8d4d25288
Bartu <bartuakyurek@outlook.com>
2025-10-02 12:42:38 +0300

    Create numeric.rs
    Declare commonly used numeric data types
    to avoid writing f32 f64 etc. all the time explicitly

---
+++ src/numeric.rs
@@ -0,0 +1,13 @@
+/*
+
+Declare numeric types used throughout this repo.
+
+@date: 2 Oct, 2025
+@author: Bartu
+*/
+
+use bevy::math::{Vec3, Vec4};
+
+pub type RGBColor = Vec3; // Distinguish from RGBA and CMYK profiles 
+pub type Int = i64;
+pub type Float = f64;
