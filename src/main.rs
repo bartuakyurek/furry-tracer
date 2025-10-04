b604c846f52a4dd3f33b96af9640c433ccb4038b
Bartu <bartuakyurek@outlook.com>
2025-10-04 13:07:44 +0300

    Add skeleton for render function
    This is just to layout how rendering logic will proceed
    after loading JSON. Currently main function is divided into
    three sections with match statements for each:
    1- load json
    2-render image
    3-write image (missing implementation)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ A simple ray tracer for CENG 795 course.
 @author: Bartu
 */
 
+use bevy_math::VectorSpace;
 use tracing::{info, debug, error, warn};
 use tracing_subscriber;
 
@@ -16,7 +17,28 @@ mod shapes;
 mod numeric;
 mod material;
 mod json_parser;
-use crate::json_parser::{parse_json795};
+use crate::{json_parser::parse_json795, scene::Scene, numeric::{Vector3}};
+
+
+
+struct ImageData {
+    pixels : Vec<Vec<Vector3>>,
+    width : usize,
+    height: usize,
+}
+
+fn render(scene: Scene) -> Result<ImageData, Box<dyn std::error::Error>>
+{
+    warn!("Assuming single camera...");
+
+    let width= 640;
+    let height =  640;
+    warn!("Use Camera.ImageResolution for width and Height.");
+
+    let pixels = vec![vec![Vector3::ZERO; width]; height];
+    let im = ImageData { pixels, width, height };
+    return Ok(im);
+}
 
 fn main() {
     
@@ -25,11 +47,25 @@ fn main() {
   
     // Parse JSON
     let json_path: &str = "./assets/test.json";
-    
-    match parse_json795(json_path) {
-        Ok(scene) => info!("Scene loaded successfully.\n {:#?}", scene),
-        Err(e) => error!("Failed to load scene: {}", e),
+    let root =  match parse_json795(json_path) {
+        Ok(root) => {
+            info!("Scene loaded successfully.\n {:#?}", root);
+            root
+        }
+        Err(e) => {
+            error!("Failed to load scene: {}", e);
+            return;
+        }
     };
 
+    // Render image and return array of RGB
+    let image_data = match render(root.scene) {
+        Ok(image_data) => {info!("Render completed."); image_data}
+        Err(e) => {error!("Failed to render scene: {}", e); return;}
+    };
+
+    // Write image data to file
+    // ...
+    warn!("Don't forget to write image data to a file.");
     info!("Furry tracer finished execution.");
 }
