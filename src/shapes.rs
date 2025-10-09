e786fc8e267fa895714c2fbe8c781d750ab317fe
Bartu <bartuakyurek@outlook.com>
2025-10-09 13:05:01 +0300

    Add compute_normal( ) to Triangle
    This is the naive implementation I believe and
    warning, it is not tested yet. There are some commented
    code of my attempts to create structures for faster memory
    access but this can be a premature optimization so I either
    will delete it soon or implement after naive version.
    
    Next goal is to learn how to write unit tests in Rust so that
    I can test this function and future ones.

--- src/shapes.rs
+++ src/shapes.rs
@@ -10,8 +10,8 @@ pub struct Triangle {
     #[serde(rename = "_id", deserialize_with = "deser_int")]
     pub id: Int,
 
-    #[serde(rename = "Indices", deserialize_with = "deser_int_vec")]
-    pub indices: Vec<Int>,
+    #[serde(rename = "Indices", deserialize_with = "deser_usize_vec")]
+    pub indices: Vec<usize>,
 
     #[serde(rename = "Material", deserialize_with = "deser_int")]
     pub material: Int,
@@ -19,6 +19,44 @@ pub struct Triangle {
     #[serde(skip)]
     pub normal: Vector3,
 }
+
+impl Triangle {
+    fn compute_normal(&mut self, vertices: &Vec<Vector3>) -> Vector3 {
+        // WARNING: Assumes triangle indices are given in counter clockwise order 
+        //
+        //    v1
+        //  /    \
+        // v2 —— v3
+        //
+        let v1 = vertices[self.indices[0]];
+        let v2 =  vertices[self.indices[1]];
+        let v3 = vertices[self.indices[2]];
+
+        let left = v1 - v2;
+        let right = v3 - v2;
+        let mut normal = right.cross(left); 
+        normal = normal.normalize();
+        debug_assert_eq!(normal.length(), 1.0);
+
+        self.normal = normal;
+        return self.normal;
+    }
+}
+
+
+//trait StructofArrays_3D {
+//    fn get_vec() -> Vec<Vector3> {
+//        // Return list of (x, y, z) coordinates
+//        // i.e. convert to array of structs 
+//    }
+//}
+
+//pub struct TriangleNormals {
+//
+//    pub xs: Vec<Float>,
+//    pub ys: Vec<Float>,
+//    pub zs: Vec<Float>,
+//}
 
 
 #[derive(Debug, Deserialize, Clone)]
