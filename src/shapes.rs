827006bd3cbb75489be4838511d0f82a2ddc80bb
Bartu <bartuakyurek@outlook.com>
2025-10-09 13:58:06 +0300

    Add TriangleNormals, remove Triangle.normal
    Dealing with geometric structures often come with a cost
    of huge structs holding data that may or may not be useful
    throughout the application. So in order to avoid bloated
    structs, here I will stick with Libigl convention (which is
    also adopted by gpytoolbox), that is to stick with simple
    matrices. So here I declare TriangleNormals which is
    supposed to be struct of arrays. Not me stepping out of
    the decision at the first step... according to libigl convention
    I would be simply writing a function like
    triangle_normals(V, F) and return N that is Matrix-like.
    
    Actually there are some modifications I want to do, like
    TriangleNormals can actually be more generic because it
    is simply holding coordinate-like data, which could be
    used for vertices as well and I've created for loop as if
    it is Python but Rust's mappings could be useful here.

--- src/shapes.rs
+++ src/shapes.rs
@@ -15,42 +15,6 @@ pub struct Triangle {
 
     #[serde(rename = "Material", deserialize_with = "deser_int")]
     pub material: Int,
-
-    #[serde(skip)]
-    pub normal: Vector3,
-}
-
-impl Triangle {
-    //fn new(i1: usize, i2: usize, i3: usize) -> Triangle {
-    //    let tri = Triangle { id: 0, 
-    //                indices: vec![i1, i2, i3], 
-    //                material: 0, 
-    //                normal: Vector3::new(0., 0., 0.) 
-    //            }
-    //    
-    //    
-    //}
-
-    fn compute_normal_naive(&mut self, vertices: &Vec<Vector3>) -> Vector3 {
-        // WARNING: Assumes triangle indices are given in counter clockwise order 
-        //
-        //    v1
-        //  /    \
-        // v2 —— v3
-        //
-        let v1 = vertices[self.indices[0]];
-        let v2 =  vertices[self.indices[1]];
-        let v3 = vertices[self.indices[2]];
-
-        let left = v1 - v2;
-        let right = v3 - v2;
-        let mut normal = right.cross(left); 
-        normal = normal.normalize();
-        debug_assert_eq!(normal.length(), 1.0);
-
-        self.normal = normal;
-        return self.normal;
-    }
 }
 
 
@@ -61,13 +25,52 @@ impl Triangle {
 //    }
 //}
 
-//pub struct TriangleNormals {
-//
-//    pub xs: Vec<Float>,
-//    pub ys: Vec<Float>,
-//    pub zs: Vec<Float>,
-//}
+fn get_tri_normal(v1: Vector3, v2: Vector3, v3: Vector3) -> Vector3{
 
+    let left = v1 - v2;
+    let right = v3 - v2;
+    let mut normal = right.cross(left); 
+    normal = normal.normalize();
+    
+    debug_assert_eq!(normal.length(), 1.0);
+    normal
+}
+
+pub struct TriangleNormals {
+
+    pub xs: Vec<Float>,
+    pub ys: Vec<Float>,
+    pub zs: Vec<Float>,
+}
+
+
+impl TriangleNormals {
+   
+    fn compute(triangles: &Vec<Triangle>, vertices: &Vec<Vector3>) -> TriangleNormals {
+        // WARNING: Assumes triangle indices are given in counter clockwise order 
+        //
+        //    v1
+        //  /    \
+        // v2 —— v3
+        //
+        let len = triangles.len();
+        let mut xs: Vec<Float> = vec![0.; len];
+        let mut ys: Vec<Float> = vec![0.; len];
+        let mut zs: Vec<Float> = vec![0.; len];
+
+        for (i, tri) in triangles.iter().enumerate()  {
+            let v1 = vertices[tri.indices[0]];
+            let v2 =  vertices[tri.indices[1]];
+            let v3 = vertices[tri.indices[2]];
+
+            let n = get_tri_normal(v1, v2, v3);
+            (xs[i], ys[i], zs[i]) = (n[0], n[1], n[2]);
+        }
+       
+        TriangleNormals { xs, ys, zs }
+    }
+    
+}
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Sphere {
@@ -98,12 +101,18 @@ mod tests {
                 Vector3::new(1., 0., 0.),
                 Vector3::new(0.5, 0.5, 0.),
         ];
-        let mut tri = Triangle { id: 0, 
+        let tri = Triangle { id: 0, 
                     indices: vec![0, 1, 2], 
                     material: 0, 
-                    normal: Vector3::new(0., 0., 0.) 
                 };
 
-        assert_eq!(tri.compute_normal_naive(&verts), Vector3::new(0., 0., 1.));
+        let triangles = vec![tri; 2];
+        let tri_normals = TriangleNormals::compute(&triangles, &verts);
+        assert_eq!(tri_normals.xs[0], 0.);
+        assert_eq!(tri_normals.ys[0], 0.);
+        assert_eq!(tri_normals.zs[0], 1.);
+        assert_eq!(tri_normals.xs[1], 0.);
+        assert_eq!(tri_normals.ys[1], 0.);
+        assert_eq!(tri_normals.zs[1], 1.);
     }
 }
