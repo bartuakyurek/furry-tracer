c0191095a7fb6f6cf04c16471be87d65027faed8
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-10-26 14:52:40 +0300

    Fix depth test
    Now the color is only updated only if the new hitrecord has smaller t
    than the previous one!

--- src/renderer.rs
+++ src/renderer.rs
@@ -11,11 +11,13 @@
     @author: Bartu
 */
 
+use std::f32::INFINITY;
+
 use tracing::{debug, info, warn};
 
 use crate::ray::Ray;
 use crate::scene::{Scene};
-use crate::numeric::{Vector3};
+use crate::numeric::{Vector3, Float};
 use crate::image::{ImageData};
 use crate::interval::{Interval};
 
@@ -29,7 +31,7 @@ pub fn render(scene: Scene) -> Result<Vec<ImageData>, Box<dyn std::error::Error>
         debug!("Nearplane corners are {:#?}", &cam.get_nearplane_corners());
         
         let (width, height) = cam.get_resolution();
-        let mut pixel_colors = vec![Vector3::ZERO; width * height]; // Colors range [0, 255], not [0, 1]
+        let mut pixel_colors = vec![scene.background_color; width * height]; // Colors range [0, 255], not [0, 1]
         
         // ------------------------ Pixel Colors ------------------------------
         // 1- Generate primary rays from camera center to pixel centers
@@ -39,13 +41,19 @@ pub fn render(scene: Scene) -> Result<Vec<ImageData>, Box<dyn std::error::Error>
         // 2- Recursive ray tracing here!
         for (i, ray) in rays.iter().enumerate(){ // TODO: parallelize with rayon, for each pixel 
             // TODO: later we'll use acceleration structures instead of checking *all* objects like this
+            let mut t_min = INFINITY as Float;
             for shape in shapes.iter() {
                 if let Some(hit_record) = shape.intersects_with(ray, &Interval::NONNEGATIVE, &scene.vertex_data){
-                    // info!("There is a hit, yippeeee!! {:?}", hit_record);
-                    let n = hit_record.normal;
-                    let color = 0.5 * (n + Vector3::new(1.0, 1.0, 1.0)); // shift to [0, 1]
-                    let color = color * 255.0; // scale to [0, 255]
-                    pixel_colors[i] = color; // red 
+                    
+                    if t_min > hit_record.ray_t {
+                        // Only update color if the hit object is closer than previous
+                        t_min = hit_record.ray_t;
+                        let n = hit_record.normal;
+                        let color = 0.5 * (n + Vector3::new(1.0, 1.0, 1.0)); // shift to [0, 1]
+                        let color = color * 255.0; // scale to [0, 255]
+                        pixel_colors[i] = color; // red 
+                    }
+                  
                 }
             
             }
