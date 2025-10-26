139bb02767cd158b0f796d1db83fb693d0e9678d
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-10-26 22:00:13 +0300

    Refactor closest_hit( )
    This is just like previous commit, just copy pasting what was inside in a
    function into another function. At this point we can start producing
    shadow rays for at least diffuse materials.

--- src/renderer.rs
+++ src/renderer.rs
@@ -15,7 +15,8 @@ use std::rc::Rc;
 use std::io::{self, Write};
 use tracing::{debug, info, warn};
 
-use crate::ray::Ray;
+use crate::dataforms::VertexData;
+use crate::ray::{HitRecord, Ray};
 use crate::scene::{Scene};
 use crate::numeric::{Vector3, Float};
 use crate::image::{ImageData};
@@ -24,22 +25,32 @@ use crate::shapes::{Shape};
 
 type ShapeList = Vec<Rc<dyn Shape>>;
 
-pub fn get_color(ray: &Ray, scene: &Scene, shapes: &ShapeList) -> Vector3 {
-    
-   let mut t_min = FloatConst::INF;
-   let t_interval = Interval::positive(scene.intersection_test_epsilon);
-   let mut color = scene.background_color;
-   for shape in shapes.iter() {
-       if let Some(hit_record) = shape.intersects_with(ray, &t_interval, &scene.vertex_data){
-           
-           if t_min > hit_record.ray_t { // Only update color if the hit object is closer than previous
+pub fn closest_hit(ray: &Ray, t_interval: &Interval, shapes: &ShapeList, vertex_data: &VertexData, rec: &mut Option<HitRecord>) {
+
+    let mut t_min = FloatConst::INF;
+    for shape in shapes.iter() { // TODO: later we'll use acceleration structures instead of checking *all* objects like this
+       if let Some(hit_record) = shape.intersects_with(ray, &t_interval, vertex_data){
+           // Update if new hit is closer 
+           if t_min > hit_record.ray_t { 
                t_min = hit_record.ray_t;
-               let n = hit_record.normal;
-               color = 0.5 * (n + Vector3::new(1.0, 1.0, 1.0)); // shift to [0, 1]
-               color = color * 255.0; // scale to [0, 255]
-               
+               *rec = Some(hit_record);
            }
        }
+   }
+}
+
+pub fn get_color(ray: &Ray, scene: &Scene, shapes: &ShapeList) -> Vector3 {
+    
+   let t_interval = Interval::positive(scene.intersection_test_epsilon);
+   let mut color = scene.background_color;
+   let mut hit_record = None;
+   closest_hit(ray, &t_interval, shapes, &scene.vertex_data, &mut hit_record);
+   
+   if let Some(hit_record) = hit_record {
+        // Update color
+        let n = hit_record.normal;
+        color = 0.5 * (n + Vector3::new(1.0, 1.0, 1.0)); // shift to [0, 1]
+        color = color * 255.0; // scale to [0, 255]
    }
    color
 }
@@ -63,7 +74,6 @@ pub fn render(scene: &Scene) -> Result<Vec<ImageData>, Box<dyn std::error::Error
 
         // 2- Recursive ray tracing here!
         for (i, ray) in eye_rays.iter().enumerate(){ // TODO: parallelize with rayon, for each pixel 
-           // TODO: later we'll use acceleration structures instead of checking *all* objects like this
            //eprint!("\rComputing {} / {}", i + 1, n_pixels); 
            //io::stdout().flush().unwrap(); TODO: how to do it with tracing crate?
             pixel_colors[i] = get_color(ray, scene, &shapes);
