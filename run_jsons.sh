bf30cb1d90cc329cc8280ee940b4eea1367445e9
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-10-26 16:21:59 +0300

    Create run_jsons.sh
    To test multiple jsons

---
+++ run_jsons.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# List of JSON files to run
+json_files=(
+    "./assets/test.json"
+    "./assets/simple.json"
+    "./assets/spheres.json"
+    "./assets/two_spheres.json"
+)
+
+for json_file in "${json_files[@]}"; do
+    if [ -f "$json_file" ]; then
+        echo "Running cargo for $json_file ..."
+        cargo run -- "$json_file"
+        echo "---------------------------------------"
+    else
+        echo "File not found: $json_file"
+    fi
+done
