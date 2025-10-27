e70a24df82edc79366106633c1797871cf4cda21
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2025-10-27 14:12:50 +0300

    Fix function signatures for concrete Material (!)
    Previously radiance function was taking a bunch of parameters, now
    they only take a LightContext struct, and work on it (however the
    implementation is missing details, so it doesn't compile atm)

--- src/material.rs
+++ src/material.rs
@@ -13,7 +13,7 @@
 */
 use std::fmt::Debug;
 use std::cmp::max;
-use tracing::{error};
+use tracing::{error, info};
 use serde::{Deserialize, de::DeserializeOwned};
 use crate::json_parser::*;
 use crate::numeric::{Float, Vector3};
@@ -25,8 +25,7 @@ use crate::lights::LightContext; // TODO: rename it to light or lighting, not li
 /// 
 /// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 pub trait Material : Debug + Send + Sync  {
-    // TODO: fn radiance() 
-
+    
     fn new_from(value: &serde_json::Value) -> Self 
     where
         Self: Sized + DeserializeOwned + Default,
@@ -39,8 +38,8 @@ pub trait Material : Debug + Send + Sync  {
             }
         }
     }
-    fn ambient_radiance(&self, ambient_light: Vector3) -> Vector3;
-    fn radiance(&self, light_context: LightContext) -> Vector3;
+    fn ambient_radiance(&self, ambient_light: Vector3) -> Vector3; // TODO: whould ambient_shade be a better name? 
+    fn radiance(&self, light_context: &LightContext) -> Vector3;
 }
 
 pub type HeapAllocMaterial = Box<dyn Material>; // Box, Rc, Arc -> Probably will be Arc when we use rayon
@@ -80,19 +79,14 @@ impl Default for DiffuseMaterial {
 
 impl DiffuseMaterial {
 
-    fn diffuse(&self, light_intensity: Vector3, light_distance: Float, w_i: Vector3, n: Vector3) -> Vector3 {
+    fn diffuse(&self, light_context: &LightContext) -> Vector3 {
         // Returns outgoing radiance (see Slides 01_B, p.73)
         let cos_theta = w_i.dot(n).max(0.0);
         let received_irradiance = light_intensity / light_distance.powi(2);
         self.diffuse_rf * cos_theta * received_irradiance
     }
 
-    fn ambient(&self, ambient_radiance: Vector3) -> Vector3 {
-        // Returns outgoing radiance (see Slides 01_B, p.75)
-        self.ambient_rf * ambient_radiance
-    }
-
-    fn specular(&self, n: Vector3, h: Vector3, received_irradiance: Vector3) -> Vector3 {
+    fn specular(&self, light_context: &LightContext) -> Vector3 {
         // Returns outgoing radiance (see Slides 01_B, p.80)
         let p = self.phong_exponent;
         let cos_a = n.dot(h).max(0.0);
@@ -102,9 +96,16 @@ impl DiffuseMaterial {
 
 
 impl Material for DiffuseMaterial{
-    //fn radiance(&self, perp_irradiance: Vector3) -> Vector3 {
-    //    self.diffuse(light_intensity, light_distance, w_i, n) + self.ambient(ambient_radiance) + self.specular(n, h, received_irradiance)
-    //}
+
+    fn radiance(&self, light_context: &LightContext) -> Vector3 {
+        self.diffuse(light_context) + self.specular(light_context)
+    }
+
+    fn ambient_radiance(&self, ambient_light: Vector3) -> Vector3 {
+        // Returns outgoing radiance (see Slides 01_B, p.75)
+        self.ambient_rf * ambient_light
+    }
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -144,9 +145,15 @@ impl Default for MirrorMaterial {
 
 
 impl Material for MirrorMaterial {
-    //fn radiance(&self, perp_irradiance: Vector3) -> Vector3 {
-    //    Vector3::new(0.0, 0., 0.0)
-    //}
+    fn ambient_radiance(&self, ambient_light: Vector3) -> Vector3 {
+        info!("Computing ambient radiance for Mirror ...");
+        Vector3::ZERO
+    }
+
+    fn radiance(&self, light_context: &LightContext) -> Vector3 {
+        info!("Computing outgoing radiance for Mirror ...");
+        Vector3::ZERO
+    }
 }
 
 // TODO: impl Default for Mirrorclear
